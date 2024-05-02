b819ffb4281c1953c804f0b1d9713f1cacfc30c1
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-02 18:42:15 -0300

    Section 8 Drag the model to rotate To see the 3D model from any angle without having to circle around it, add a drag gesture to rotate the model inside the volumetric window.

--- SeaCreatures/SeaCreatureDetailView.swift
+++ SeaCreatures/SeaCreatureDetailView.swift
@@ -12,9 +12,32 @@ import RealityKitContent
 struct SeaCreatureDetailView: View {
     
     let modelName: String
+    @State private var horizontalRotation = CGFloat.zero
+    @State private var verticalRotation = CGFloat.zero
+    
+    @State private var endHorizontalRotation = CGFloat.zero
+    @State private var endVerticalRotation = CGFloat.zero
+    
     
     var body: some View {
         Model3D(named: modelName, bundle: realityKitContentBundle)
+            .rotation3DEffect(
+                .degrees(horizontalRotation), axis: .y
+            )
+            .rotation3DEffect(
+                .degrees(verticalRotation), axis: .x
+            )
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        horizontalRotation = value.translation.width + endHorizontalRotation
+                        verticalRotation = value.translation.height + endVerticalRotation
+                    })
+                    .onEnded({ value in
+                        endHorizontalRotation = value.translation.width
+                        endVerticalRotation = value.translation.height
+                    })
+            )
     }
 }
 
