0231a752fae804fcce13c561672fabe066613957
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-02 18:12:56 -0300

    Section 6 Set up the detail view In the navigation split view’s detail view, show an image of a creature when it’s selected from the list. Then, add a button in the toolbar. Later, you’ll use this button to view the selected creature in a 3D volumetric window.

--- SeaCreatures/ContentView.swift
+++ SeaCreatures/ContentView.swift
@@ -31,7 +31,20 @@ struct ContentView: View {
             }
             .navigationTitle("Sea Creatures")
         } detail: {
-            
+            if let selectedCreature {
+                Model3D(named: selectedCreature.nodelName, bundle: realityKitContentBundle)
+                    .navigationTitle(selectedCreature.name)
+                    .toolbar {
+                        Button {
+                            
+                        } label: {
+                            Text("View \(selectedCreature.name)")
+                        }
+
+                    }
+            } else {
+                Text("Select a sea creature")
+            }
         }
 
     }
